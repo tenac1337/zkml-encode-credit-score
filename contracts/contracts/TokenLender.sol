1d3ba158ac44d1560fb9e5fc5b6d5be3729b0788
Howard <hwrdtm@users.noreply.github.com>
2025-03-20 05:36:01 -0700

    WIP: Contracts (#1)

---
+++ contracts/contracts/TokenLender.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "./Verifier.sol";
+
+contract TokenLender is Halo2Verifier {
+
+    address public owner;
+
+    error InvalidProof();
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function submitUnderwritingDecision(
+        bytes calldata proof,
+        uint256[] calldata instances
+    ) external onlyOwner {
+        // Verify that the classification was valid using the proof.
+        if (!verifyProof(proof, instances)) {
+            revert InvalidProof();
+        }
+
+        // TODO: Tie to the user wallet address
+
+        // Get the user address
+        address user = address(0x1);
+
+        // Send funds to the user
+        payable(user).transfer(100);
+    }
+}
